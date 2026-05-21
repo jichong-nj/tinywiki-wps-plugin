634008e75bd558197696a777f2908bfa524d2c5a
jichong-nj <jichong79@gmail.com>
2026-05-21 17:18:03 +0800

    我的生产环境地址

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 npm install
 wpsjs build
-wpsjs publish -s http://localhost/wps-plugin/
+wpsjs publish -s http://10.20.134.53/wps-plugin/
 cp wps-addon-publish/publish.html wps-addon-build/
 tar -zcvf wps-plugin.tar.gz wps-addon-build 
 sudo chmod 755 wps-plugin.tar.gz
