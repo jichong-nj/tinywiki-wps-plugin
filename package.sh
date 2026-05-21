e62fc65df28c94b9d71748ed9abd0bbbb04b5064
jichong-nj <jichong79@gmail.com>
2026-05-21 15:30:31 +0800

    打包修改

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 npm install
 wpsjs build
 wpsjs publish -s http://localhost/wps-plugin/
-cp wps-addon-publish/publish.html wps-addon-publish/
-tar -zcvf wps-plugin.tar.gz wps-addon-publish 
+cp wps-addon-publish/publish.html wps-addon-build/
+tar -zcvf wps-plugin.tar.gz wps-addon-build 
